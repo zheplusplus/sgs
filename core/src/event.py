70bff81afbab87e59c9407ec385b50e2c4eb2d18
Neuron Teckid <lene13@gmail.com>
2011-11-14 22:42:49 +0800

    more events

--- core/src/event.py
+++ core/src/event.py
@@ -21,10 +21,22 @@ class EventList:
     def add(self, event):
         self.events.append(event)
 
-def cards_to_msg(cards, to_msg):
-    return reduce(lambda l, c: l + [to_msg(c)], cards, [])
+def cards_to_msg(cards):
+    return reduce(lambda l, c: l + [{
+                                       'name': c.name,
+                                       'rank': c.rank,
+                                       'suit': c.suit,
+                                   }], cards, [])
 
-class DealCard(Event):
+def cards_to_msg_include_id(cards):
+    return reduce(lambda l, c: l + [{
+                                       'id': c.card_id,
+                                       'name': c.name,
+                                       'rank': c.rank,
+                                       'suit': c.suit,
+                                   }], cards, [])
+
+class DealCards(Event):
     player = None
     cards = []
 
@@ -43,15 +55,10 @@ class DealCard(Event):
     def as_log(self):
         return [{
             'player_id': self.player.player_id,
-            'get': cards_to_msg(self.cards, lambda c: {
-                                                'id': c.card_id,
-                                                'name': c.name,
-                                                'rank': c.rank,
-                                                'suit': c.suit,
-                                            }),
+            'get': cards_to_msg_include_id(self.cards),
         }]
 
-class DiscardCard(Event):
+class DiscardCards(Event):
     player = None
     cards = []
 
@@ -62,9 +69,56 @@ class DiscardCard(Event):
     def as_log(self):
         return [{
             'player_id': self.player.player_id,
-            'discard': cards_to_msg(self.cards, lambda c: {
-                                                    'name': c.name,
-                                                    'rank': c.rank,
-                                                    'suit': c.suit,
-                                                }),
+            'discard': cards_to_msg(self.cards),
+        }]
+
+class UseCards(Event):
+    user = None
+    target = None
+    action = ''
+    cards = []
+
+    def __init__(self, user, target, action, cards):
+        self.user = user
+        self.target = target
+        self.action = action
+        self.cards = cards
+
+    def as_log(self):
+        return [{
+            'user': self.user.player_id,
+            'target': self.target.player_id,
+            'action': self.action,
+            'use': cards_to_msg(self.cards),
+        }]
+
+class ShowCards(Event):
+    player = None
+    cards = []
+
+    def __init__(self, player, cards):
+        self.player = player
+        self.cards = cards
+
+    def as_log(self):
+        return [{
+            'player_id': self.player.player_id,
+            'show': cards_to_msg(self.cards),
+        }]
+
+class Damage(Event):
+    victim = None
+    damage = 0
+    category = ''
+
+    def __init__(self, victim, damage, category='normal'):
+        self.victim = victim
+        self.damage = damage
+        self.category = category
+
+    def as_log(self):
+        return [{
+            'victim': self.victim.player_id,
+            'damage': self.damage,
+            'category': self.category,
         }]
