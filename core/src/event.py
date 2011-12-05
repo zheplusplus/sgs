18c92cbb0f0952211b1248777da789ee3d4905af
Neuron Teckid <lene13@gmail.com>
2011-12-05 22:44:08 +0800

    add type info to events

--- core/src/event.py
+++ core/src/event.py
@@ -1,17 +1,27 @@
 class Event:
+    def _add_type(self, data):
+        data['type'] = self.__class__.__name__
+        return data
+
+    def as_log(self):
+        return self._add_type(self._as_log())
+
     def serialize(self, player_token):
-        return self.as_log()
+        return self._add_type(self._serialize(player_token))
+
+    def _serialize(self, player_token):
+        return self._as_log()
 
 class EventList:
     def __init__(self):
         self.events = []
 
     def serialize(self, token, prev_event_id):
-        return reduce(lambda l, e: l + e.serialize(token)
+        return reduce(lambda l, e: l + [e.serialize(token)]
                     , self.events[prev_event_id:], [])
 
     def as_log(self):
-        return reduce(lambda l, e: l + e.as_log(), self.events, [])
+        return reduce(lambda l, e: l + [e.as_log()], self.events, [])
 
     def add(self, event):
         self.events.append(event)
@@ -36,38 +46,38 @@ class DealCards(Event):
         self.player = player
         self.cards = cards
 
-    def serialize(self, player_token):
+    def _serialize(self, player_token):
         if player_token == self.player.token:
             return self.as_log()
-        return [{
+        return {
             'player_id': self.player.player_id,
             'get': len(self.cards),
-        }]
+        }
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'player_id': self.player.player_id,
             'get': cards_to_msg_include_id(self.cards),
-        }]
+        }
 
 class DiscardCards(Event):
     def __init__(self, player, cards):
         self.player = player
         self.cards = cards
 
-    def serialize(self, player_token):
+    def _serialize(self, player_token):
         if player_token == self.player.token:
             return self.as_log()
-        return [{
+        return {
             'player_id': self.player.player_id,
             'discard': cards_to_msg(self.cards),
-        }]
+        }
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'player_id': self.player.player_id,
             'discard': cards_to_msg_include_id(self.cards),
-        }]
+        }
 
 class UseCardsForPlayers(Event):
     def __init__(self, user, targets_ids, action, cards):
@@ -76,53 +86,53 @@ class UseCardsForPlayers(Event):
         self.action = action
         self.cards = cards
 
-    def serialize(self, player_token):
+    def _serialize(self, player_token):
         if player_token == self.user.token:
             return self.as_log()
-        return [{
+        return {
             'user': self.user.player_id,
             'targets': self.targets_ids,
             'action': self.action,
             'use': cards_to_msg(self.cards),
-        }]
+        }
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'user': self.user.player_id,
             'targets': self.targets_ids,
             'action': self.action,
             'use': cards_to_msg_include_id(self.cards),
-        }]
+        }
 
 class PlayCards(Event):
     def __init__(self, player, cards):
         self.player = player
         self.cards = cards
 
-    def serialize(self, player_token):
+    def _serialize(self, player_token):
         if player_token == self.player.token:
             return self.as_log()
-        return [{
+        return {
             'player': self.player.player_id,
             'play': cards_to_msg(self.cards),
-        }]
+        }
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'player': self.player.player_id,
             'play': cards_to_msg_include_id(self.cards),
-        }]
+        }
 
 class ShowCards(Event):
     def __init__(self, player, cards):
         self.player = player
         self.cards = cards
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'player_id': self.player.player_id,
             'show': cards_to_msg(self.cards),
-        }]
+        }
 
 class Damage(Event):
     def __init__(self, victim, damage, category):
@@ -130,9 +140,9 @@ class Damage(Event):
         self.damage = damage
         self.category = category
 
-    def as_log(self):
-        return [{
+    def _as_log(self):
+        return {
             'victim': self.victim.player_id,
             'damage': self.damage,
             'category': self.category,
-        }]
+        }
