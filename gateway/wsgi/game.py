bf9db49b399c48c964f6b377ac697da10516b00c
Neuron Teckid <lene13@gmail.com>
2012-02-13 10:37:06 +0800

    refactor game

--- gateway/wsgi/game.py
+++ gateway/wsgi/game.py
@@ -15,48 +15,57 @@ from gateway.wsgi import log
 class GameRoom:
     def __init__(self):
         self.players_tokens = []
-        self.response = self.before_game_start
+        self.host = ''
+        self.handlers = {
+                            '/ctrl/add': self.add_player,
+                            '/ctrl/start': self.start,
+                            '/info/status': self.game_status,
+                            '/info/events': self.get_events,
+                            '/info/hint': self.get_hint,
+                            '/act': self.player_act,
+                        }
+        self.game_started = False
 
-    def game_status(self, path, request_body):
-        if path == '/info/status':
-            return {
-                       'code': ret_code.OK,
-                       'players': len(self.players_tokens),
-                       'started': 0 if self.response == self.before_game_start
-                                    else 1,
-                   }
-        return { 'code': 404 }
-
-    def before_game_start(self, path, request_body):
-        if path == '/ctrl/start':
-            return self.start(request_body)
-        if path == '/ctrl/add':
-            return self.add_player(time.time(), request_body)
-        return self.game_status(path, request_body)
-
-    def after_game_start(self, path, request_body):
+    def response(self, path, request_body):
+        if not path in self.handlers:
+            return { 'code': 404 }
         try:
-            if path == '/act':
-                return self.player_act(eval(request_body, dict(), dict()))
-            if path == '/info/events':
-                return {
-                           'code': 200,
-                           'events': self.get_events(eval(request_body,
-                                                     dict(), dict()))
-                       }
-            if path == '/info/hint':
-                hint = self.gc.hint()
-                hint['code'] = 200
-                return hint
-            return self.game_status(path, request_body)
+            return self.handlers[path](request_body)
+        except ValueError, e:
+            return {
+                       'code': ret_code.BAD_REQUEST,
+                       'reason': e.message,
+                   }
+        except KeyError, e:
+            return {
+                       'code': ret_code.BAD_REQUEST,
+                       'reason': ret_code.BR_MISSING_ARG % e.message,
+                   }
         except (NameError, SyntaxError), e:
             return {
                        'code': ret_code.BAD_REQUEST,
-                       'reason': 'syntax error',
+                       'reason': e.message,
                    }
 
-    def add_player(self, time, name):
-        token = sha256(name + str(time)).hexdigest()
+    def _check_game_not_started(self):
+        if self.game_started:
+            raise ValueError('Game started')
+
+    def _check_game_started(self):
+        if not self.game_started:
+            raise ValueError('Game not started')
+
+    def game_status(self, request_body):
+        return {
+                   'code': ret_code.OK,
+                   'players': len(self.players_tokens),
+                   'started': 1 if self.game_started else 0,
+                   'host': 1 if self.host == request_body else 0,
+               }
+
+    def add_player(self, request_body):
+        self._check_game_not_started()
+        token = sha256(request_body + str(time.time())).hexdigest()
         log.i('Add player:token=' + token)
         if len(self.players_tokens) == 0:
             log.i('Be host:token=' + token)
@@ -68,6 +77,7 @@ class GameRoom:
                }
 
     def start(self, token):
+        self._check_game_not_started()
         if len(self.players_tokens) < 2:
             return {
                        'code': ret_code.BAD_REQUEST,
@@ -85,19 +95,25 @@ class GameRoom:
                               pc, ActionStack())
         for token in self.players_tokens: pc.add_player(Player(token, 4))
         self.gc.start()
-        self.response = self.after_game_start
+        self.game_started = True
         return { 'code': ret_code.OK }
 
-    def get_events(self, args):
-        try:
-            return self.gc.get_events(args['token'], args['previous event id'])
-        except KeyError, e:
-            return {
-                       'code': ret_code.BAD_REQUEST,
-                       'reason': ret_code.BR_MISSING_ARG % e.message,
-                   }
+    def get_hint(self, req):
+        self._check_game_started()
+        hint = self.gc.hint()
+        hint['code'] = 200
+        return hint
 
-    def player_act(self, args):
-        return self.gc.player_act(args)
+    def get_events(self, request_body):
+        self._check_game_started()
+        args = eval(request_body, dict(), dict())
+        return {
+                   'code': 200,
+                   'events': self.gc.get_events(args['token'],
+                                                args['previous event id']),
+               }
+
+    def player_act(self, request_body):
+        return self.gc.player_act(eval(request_body, dict(), dict()))
 
 game_room = GameRoom()
