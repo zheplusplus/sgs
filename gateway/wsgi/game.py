352b87228972194582915d6909da31b6625d16f8
Neuron Teckid <Lene13@gmail.com>
2012-03-03 15:43:06 +0800

    refactor game room

--- gateway/wsgi/game.py
+++ gateway/wsgi/game.py
@@ -8,46 +8,17 @@ class GameRoom:
     def __init__(self):
         self.players_tokens = []
         self.host = ''
-        self.handlers = {
-                            '/ctrl/add': self.add_player,
-                            '/ctrl/exit': self.player_exit,
-                            '/ctrl/start': self.start,
-                            '/info/status': self.game_status,
-                            '/info/events': self.get_events,
-                            '/info/hint': self.get_hint,
-                            '/info/checktoken': self.check_token,
-                            '/act': self.player_act,
-                        }
         self.game_started = False
 
-    def response(self, path, request_body):
-        if not path in self.handlers:
-            return { 'code': 404 }
-        try:
-            return self.handlers[path](request_body)
-        except ValueError, e:
-            return {
-                       'code': ret_code.BAD_REQUEST,
-                       'reason': e.message,
-                   }
-        except KeyError, e:
-            return {
-                       'code': ret_code.BAD_REQUEST,
-                       'reason': ret_code.BR_MISSING_ARG % e.message,
-                   }
-        except (NameError, SyntaxError), e:
-            return {
-                       'code': ret_code.BAD_REQUEST,
-                       'reason': 'Syntax error: %s' % e.message,
-                   }
-
     def check_token(self, request_body):
+        token = request_body['token']
         return {
                    'code': ret_code.OK,
-                   'in': 1 if request_body in self.players_tokens else 0,
+                   'in': 1 if token in self.players_tokens else 0,
                }
 
-    def player_exit(self, token):
+    def player_exit(self, request_body):
+        token = request_body['token']
         if not token in self.players_tokens:
             raise ValueError('Not joined in')
         self.players_tokens.remove(token)
@@ -64,18 +35,19 @@ class GameRoom:
             raise ValueError('Game not started')
 
     def game_status(self, request_body):
+        token = request_body['token'] if 'token' in request_body else ''
         return {
                    'code': ret_code.OK,
                    'players': len(self.players_tokens),
                    'started': 1 if self.game_started else 0,
-                   'host': 1 if self.host == request_body else 0,
+                   'host': 1 if self.host == token else 0,
                }
 
     def add_player(self, request_body):
         self._check_game_not_started()
         if 8 == len(self.players_tokens):
             raise ValueError('room full')
-        token = sha256(request_body + str(time.time())).hexdigest()
+        token = sha256(str(request_body) + str(time.time())).hexdigest()
         if len(self.players_tokens) == 0:
             self.host = token
         self.players_tokens.append(token)
@@ -84,8 +56,9 @@ class GameRoom:
                    'token': token,
                }
 
-    def start(self, token):
+    def start(self, request_body):
         self._check_game_not_started()
+        token = request_body['token']
         if len(self.players_tokens) < 2:
             return {
                        'code': ret_code.BAD_REQUEST,
@@ -102,11 +75,10 @@ class GameRoom:
 
     def get_hint(self, request_body):
         self._check_game_started()
-        return self.gc.hint(request_body)
+        return self.gc.hint(request_body['token'])
 
-    def get_events(self, request_body):
+    def get_events(self, args):
         self._check_game_started()
-        args = eval(request_body, dict(), dict())
         return {
                    'code': 200,
                    'events': self.gc.get_events(args['token'],
@@ -114,6 +86,6 @@ class GameRoom:
                }
 
     def player_act(self, request_body):
-        return self.gc.player_act(eval(request_body, dict(), dict()))
+        return self.gc.player_act(request_body)
 
 game_room = GameRoom()
--- gateway/wsgi/server.py
+++ gateway/wsgi/server.py
@@ -1,6 +1,8 @@
 import os
 from wsgiref.simple_server import make_server
+import simplejson
 
+import core.src.ret_code as ret_code
 import game
 import static_files
 
@@ -9,6 +11,7 @@ def start():
 
 def plain_headers():
     return [('Content-type', 'text/plain')]
+
 RESPONSE_MAPPING = {
                        200: lambda sr: sr('200 OK', plain_headers()),
                        400: lambda sr: sr('400 Bad Request', plain_headers()),
@@ -17,16 +20,52 @@ RESPONSE_MAPPING = {
 
 def app(env, start_response):
     path = env['PATH_INFO']
-    if '/' == path:
+    if path == '/':
         start_response('200 OK', [('Content-type', 'text/html')])
         return read_index()
     if path.startswith('/static/'):
         return static_files.serve(path[1:], start_response)
-    request_body_size = int(env['CONTENT_LENGTH'])
-    request_body = env['wsgi.input'].read(request_body_size)
-    response = game.game_room.response(path, request_body)
-    RESPONSE_MAPPING[response['code']](start_response)
-    return [str(response)]
+    if path == '/action':
+        request_body_size = int(env['CONTENT_LENGTH'])
+        request_body = env['wsgi.input'].read(request_body_size)
+        response = game_response(request_body)
+        RESPONSE_MAPPING[response['code']](start_response)
+        return [simplejson.dumps(response)]
+    RESPONSE_MAPPING[404](start_response)
+    return ['']
+
+def game_response(request_body):
+    try:
+        request_body = simplejson.loads(request_body)
+        handlers = {
+            'ctrl/add': game.game_room.add_player,
+            'ctrl/exit': game.game_room.player_exit,
+            'ctrl/start': game.game_room.start,
+            'info/status': game.game_room.game_status,
+            'info/events': game.game_room.get_events,
+            'info/hint': game.game_room.get_hint,
+            'info/checktoken': game.game_room.check_token,
+            'act': game.game_room.player_act,
+        }
+        path = request_body['controller']
+        if not path in handlers:
+            return { 'code': 404 }
+        return handlers[path](request_body)
+    except ValueError, e:
+        return {
+                   'code': ret_code.BAD_REQUEST,
+                   'reason': e.message,
+               }
+    except KeyError, e:
+        return {
+                   'code': ret_code.BAD_REQUEST,
+                   'reason': ret_code.BR_MISSING_ARG % e.message,
+               }
+    except (NameError, SyntaxError), e:
+        return {
+                   'code': ret_code.BAD_REQUEST,
+                   'reason': 'Syntax error: %s' % e.message,
+               }
 
 def read_index():
     path = os.path.join(os.path.dirname(__file__), 'static/index.html')
